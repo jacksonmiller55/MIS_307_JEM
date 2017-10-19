d738a3b3977be580878b6eae04de6bff9d078915
Miller, Jackson H <jacksonmiller55@gmail.com>
2017-10-18 23:06:17 -0500

    Added command interface
    Added option for printing all the rooms and quiting the program.

--- src/HotelManagmentSystem.java
+++ src/HotelManagmentSystem.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Code for HW5
@@ -43,12 +42,37 @@ public class HotelManagmentSystem {
 			} catch (java.sql.SQLException e) {
 			}
 			
+			//People that have downloaded the code and are able to use it.
 			System.out.println("People that have cloned the repository: Jackson, Elias");
 			
+			//Creates and loads the table
 			createTable(stat);
-
 			loadRooms(stat, filename);
-			printTable(stat);
+			
+			String selection = null;
+			Scanner input = new Scanner(System.in);
+			boolean firstRun = true;
+			boolean continueToRun = true;
+			while(continueToRun) {
+				//Adds 2 lines for spacing after the first time the program is run.
+				if (!firstRun) {
+					System.out.println();
+					System.out.println();
+				}
+				selection = printSelectionOptions(input);
+
+				if (selection.equals("Q")) {
+					continueToRun = quitProgram();
+				
+				} else if (selection.equals("P")) {
+					printTable(stat);
+				
+				}
+				//Tells the program that this is no longer the first run.
+				firstRun = false;
+			}
+			input.close();
+			
 			stat.execute("DROP TABLE Room");
 		}
 	}
@@ -138,5 +162,41 @@ public class HotelManagmentSystem {
 			System.out.println("");
 		}
 		result.close();
+	}
+	
+	public static boolean quitProgram() {
+		System.out.println("You have quit the program.");
+		return false;
+	}
+	
+	public static String printSelectionOptions(Scanner input) {
+		String selection = null;
+		boolean correctInput = false;
+
+		System.out.println("Select from the following options");
+		System.out.println("(Q) Quit");
+		System.out.println("(P) Print all of the rooms");
+
+		selection = input.next();
+
+		while (!correctInput) {
+
+			if (selection.length() > 2) {
+				System.out.println("Please enter only one character to select the appropriat option.");
+				selection = input.next();
+			} else if (!Character.isAlphabetic(selection.charAt(0))) {
+				System.out.println(
+						"Please enter only the selected character to select the appropriat option. Numbers will not be accepted.");
+				selection = input.next();
+			} else if ((selection.toUpperCase().equals("Q"))
+					|| (selection.toUpperCase().equals("P"))) 
+			{
+				correctInput = true;
+			} else {
+				System.out.println("The Character must match one of the appropriat options.");
+				selection = input.next();
+			}
+		}
+		return selection.toUpperCase();
 	}
 }
